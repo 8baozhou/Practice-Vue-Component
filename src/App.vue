9ee1add2fa39ccafb0c567eb3b77886c88d1dd26
8baozhou <timchiu95@gmail.com>
2019-07-19 11:28:13 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+        id="app"
+        class="min-h-screen bg-gray-700 p-8"
+    >
+        <div class="max-w-md mx-auto"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    components: {}
+}
+</script>
+
+<style src="@/assets/css/main.css">
+</style>
