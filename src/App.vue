77ebb561248ee1a101d616917c5bbdc23ff2deaf
8baozhou <timchiu95@gmail.com>
2019-07-19 12:13:35 +0800

    练习 v-model

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,19 @@
         id="app"
         class="min-h-screen bg-gray-700 p-8"
     >
-        <div class="max-w-md mx-auto"></div>
+        <div class="max-w-md mx-auto">
+            <BaseForm />
+        </div>
     </div>
 </template>
 
 <script>
+import BaseForm from '@/components/BaseForm.vue'
 export default {
     name: 'app',
-    components: {}
+    components: {
+        BaseForm,
+    }
 }
 </script>
 
---
+++ src/components/BaseForm.vue
@@ -0,0 +1,59 @@
+<template>
+    <form
+        class="card"
+        @submit.prevent="submit"
+    >
+        <h1 class="text-2xl font-bold mb-6">练习 1</h1>
+        <label class="block mb-6">
+            <span class="form-label mb-2">手机号码</span>
+            <input
+                class="form-input"
+                placeholder="123-4567-8910"
+                type="number"
+                v-model="phone"
+            />
+        </label>
+        <div class="mb-8">
+            <span class="form-label mb-2">验证码登录</span>
+            <div class="flex justify-between items-center">
+                <p class="mr-4">暂不设置密码，登录时使用验证码进行登录</p>
+                <span
+                    class="toggle"
+                    role="checkbox"
+                    tabindex="0"
+                    :aria-checked="codeLoginReceive.toString()"
+                    @click="toggle"
+                    @keydown.space.prevent="toggle"
+                ></span>
+            </div>
+        </div>
+        <div class="text-right">
+            <button
+                type="submit"
+                class="btn btn-blue"
+            >注册</button>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            phone: null,
+            codeLoginReceive: true
+        }
+    },
+    methods: {
+        submit() {
+            console.log('submit', {
+                phone: this.phone,
+                codeLoginReceive: this.codeLoginReceive
+            })
+        },
+        toggle() {
+            this.codeLoginReceive = !this.codeLoginReceive
+        }
+    },
+}
+</script>
